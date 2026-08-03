c822b226f5e1305064dc8645103bf81ae8ff0cd3
benmch0 <ben21mchale@gmail.com>
2026-08-03 23:08:02 +0100

    Solution for 71A, Way too long words

---
+++ toolongwords.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   string word;
+   std::cin >> n;
+
+   while(n--)
+   {
+      std::cin >> word;
+      if (word.length() > 10)
+      {
+         cout << word[0] << (word.length() - 2) << word[word.length() - 1] << "\n";
+      }
+      else
+      {
+         cout << word << "\n";
+      }
+   }
+}
